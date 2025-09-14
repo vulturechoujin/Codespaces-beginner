41be8d9a7ace09bcda853235c23b3924ab7927f7
Duong Nghiep Hoang <134819645+vulturechoujin@users.noreply.github.com>
2025-09-14 16:34:03 +0800

    Change image and alt-text

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Will Serfort.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "My favorite character from Wistoria: Wand and Sword. He always stand up tall upon challanges.";
 
 /**
  * Project list
